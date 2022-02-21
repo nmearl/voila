324c81bb695eb72d4f362ad7610d85d6da8cec49
Nicholas Earl <contact@nicholasearl.me>
2022-02-21 13:36:55 -0600

    Force pywwt shim on start

--- voila/app.py
+++ voila/app.py
@@ -135,6 +135,8 @@ class Voila(Application):
         'strip_sources': 'VoilaConfiguration.strip_sources',
         'template': 'VoilaConfiguration.template',
         'theme': 'VoilaConfiguration.theme'
+        'pool_size': 'VoilaConfiguration.default_pool_size',
+        'connection_dir_root': 'Voila.connection_dir_root'
     }
     classes = [
         VoilaConfiguration,
@@ -469,7 +471,8 @@ class Voila(Application):
                 'comm_msg',
                 'comm_info_request',
                 'kernel_info_request',
-                'shutdown_request'
+                'shutdown_request',
+                'wwtkdr_claim_key'
             ]
         )
 
@@ -496,6 +499,18 @@ class Voila(Application):
         self.app.settings.update(self.tornado_settings)
 
         handlers = []
+
+        # Force the wwt relay shim
+        from wwt_kernel_data_relay import load_jupyter_server_extension
+
+        class AppWrapper:
+            settings = self.app.settings
+
+            def add_handlers(self, root, handles):
+                handlers.extend(handles)
+
+        self.web_app = AppWrapper()
+        load_jupyter_server_extension(self)
 
         handlers.extend([
             (url_path_join(self.server_url, r'/api/kernels/%s' % _kernel_id_regex), KernelHandler),
